d123b1cb935c70d7d90d07ada6fe7961b16e7b17
Bryan Taylor <bryant486@gmail.com>
2021-12-17 20:24:57 -0500

    Updates 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,30 @@
 import React from "react"
 
+import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import styled from "styled-components"
+import MarkAnimation from "../components/MarkAnimation"
+
+const PageTitle = styled.h1`
+  color: #fff;
+`
+const HomeLink = styled(Link)`
+  color: #fff;
+
+  &:hover {
+    animation-name: ${MarkAnimation};
+    animation-duration: 0.75s;
+    animation-iteration-count: infinite;
+  }
+`
 
 const NotFoundPage = () => (
   <Layout>
     <Seo title="404: Not found" />
-    <h1>nothing here</h1>
+    <PageTitle>
+      nothing here take the trip <HomeLink to="/">home</HomeLink>
+    </PageTitle>
   </Layout>
 )
 
